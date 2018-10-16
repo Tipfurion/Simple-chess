df8dbe63f85c9a5a71886f0e8a672c870a2cdf5c
Ruslan <Ovezov3210@gmail.com>
2018-10-16 23:11:25 +0300

    2

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ const ctx = canvas.getContext("2d");
 const blackCellColor="#595959";
 const whiteCellColor="white";
 const activeColor="#237716";
-var clickX;
-var clickY;
-var prevActiveCell;
-var cellWidth=canvas.clientWidth/8;
-var cellHeight=canvas.clientHeight/8;
-var chessPlate=
+let clickX;
+let clickY;
+let prevActiveCell;
+let cellWidth=canvas.clientWidth/8;
+let cellHeight=canvas.clientHeight/8;
+let chessPlate=
 [A1={},B1={},C1={},D1={},E1={},F1={},G1={},H1={},      
  A2={},B2={},C2={},D2={},E2={},F2={},G2={},H2={},
  A3={},B3={},C3={},D3={},E3={},F3={},G3={},H3={},
@@ -20,6 +20,8 @@ var chessPlate=
 ]
 console.log("start");
 drawField();
+
+setInterval(ping,1000);
 function drawField()
 {
 
@@ -82,44 +84,44 @@ offsetX+=cellWidth;
 
 }
 drawText();
+
+}
 function drawText(){
-ctx.font = "30px Arial";
-ctx.fillStyle = "green";
-let letter;
-for(let i=0; i<8;i++)
-{
-    switch(i)
+    ctx.font = "30px Arial";
+    ctx.fillStyle = "green";
+    let letter;
+    for(let i=0; i<8;i++)
     {
-        case 0:letter="A";break;
-        case 1:letter="B";break;
-        case 2:letter="C";break;
-        case 3:letter="D";break;
-        case 4:letter="E";break;
-        case 5:letter="F";break;
-        case 6:letter="G";break;
-        case 7:letter="H";break; 
+        switch(i)
+        {
+            case 0:letter="A";break;
+            case 1:letter="B";break;
+            case 2:letter="C";break;
+            case 3:letter="D";break;
+            case 4:letter="E";break;
+            case 5:letter="F";break;
+            case 6:letter="G";break;
+            case 7:letter="H";break; 
+        }
+    ctx.strokeText(letter,chessPlate[i].coords.x-cellWidth+50,chessPlate[i].coords.y);
+    ctx.fillText(letter,chessPlate[i].coords.x-cellWidth+50,chessPlate[i].coords.y);
     }
-ctx.strokeText(letter,chessPlate[i].coords.x-cellWidth+50,chessPlate[i].coords.y);
-ctx.fillText(letter,chessPlate[i].coords.x-cellWidth+50,chessPlate[i].coords.y);
-}
-let counter=0;
-ctx.font = "30px Arial";
-ctx.fillStyle = "green";
-
-for(let i=0; i<7;i++)
-{
-        
-        ctx.strokeText(i+1,chessPlate[counter].coords.x-cellWidth,chessPlate[counter].coords.y); 
-        ctx.fillText(i+1,chessPlate[counter].coords.x-cellWidth,chessPlate[counter].coords.y);
-        counter+=8;
-        
-        
-}
-ctx.strokeText(8,chessPlate[56].coords.x-cellWidth,chessPlate[56].coords.y); 
-ctx.fillText(8,chessPlate[56].coords.x-cellWidth,chessPlate[56].coords.y);
-}
-}
-
+    let counter=0;
+    ctx.font = "30px Arial";
+    ctx.fillStyle = "green";
+    
+    for(let i=0; i<7;i++)
+    {
+            
+            ctx.strokeText(i+1,chessPlate[counter].coords.x-cellWidth,chessPlate[counter].coords.y); 
+            ctx.fillText(i+1,chessPlate[counter].coords.x-cellWidth,chessPlate[counter].coords.y);
+            counter+=8;
+            
+            
+    }
+    ctx.strokeText(8,chessPlate[56].coords.x-cellWidth,chessPlate[56].coords.y); 
+    ctx.fillText(8,chessPlate[56].coords.x-cellWidth,chessPlate[56].coords.y);
+    }
 function cellClick()
 {
     
@@ -147,7 +149,14 @@ if(clickX>=chessPlate[i].coords.x-cellWidth && clickX <= chessPlate[i].coords.x 
 
     ctx.fillStyle = activeColor;
     ctx.fillRect(chessPlate[i].coords.x-cellWidth, chessPlate[i].coords.y-cellHeight, cellWidth,cellHeight);
-
+    drawText();
+    for(let i=0; i<chessPlate.length; i++)
+    {
+        if(chessPlate[i].figure!=null)
+        {
+            chessPlate[i].figure.draw();
+        }
+    }
 }
 }
 }
@@ -156,12 +165,51 @@ canvas.addEventListener('click', function(event) {
     let rect = canvas.getBoundingClientRect();
     clickX = event.clientX - rect.left;
     clickY = event.clientY - rect.top;
-   // console.log("x: " +  clickX + " y: " + clickY); 
+    console.log("x: " +  clickX + " y: " + clickY); 
     cellClick()
 }, false);
+
 
 function ping()
 {
     console.log('ping');
 }
-setInterval(ping,1000);
+
+class Rook 
+{
+draw(x,y)
+{
+    Rook.sprite.onload=function()
+{
+ctx.drawImage(Rook.sprite,x ,y);
+console.log('drawed')
+} 
+}
+constructor(color, x, y)
+{
+Rook.sprite=new Image();   
+this.color=color;
+this.x=x;
+this.y=y;
+if(this.color=="white")
+{
+    Rook.sprite.src="img/whiteRook.png"
+}
+else
+{
+    Rook.sprite.src="img/blackRook.png"
+}
+this.draw(this.x-cellWidth,this.y-cellHeight);
+
+}
+
+    
+}
+function setFigures()
+{
+
+let whiteRook=new Rook("white",chessPlate[0].coords.x, chessPlate[0].coords.y);
+chessPlate[0].figure=whiteRook;
+
+}
+setFigures();
